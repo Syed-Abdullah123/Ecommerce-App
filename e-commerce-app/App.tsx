b86ba190aa10db9f750b8678a95f16cbfd4e84ea
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-05-04 22:11:49 +0500

    wrapped the app with stripe provider

--- e-commerce-app/App.tsx
+++ e-commerce-app/App.tsx
@@ -3,13 +3,16 @@ import { StyleSheet, View } from "react-native";
 import Navigation from "./src/navigation";
 import { Provider } from "react-redux";
 import { store } from "./src/store";
+import { StripeProvider } from "@stripe/stripe-react-native";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Navigation />
-      <StatusBar style="auto" />
-    </Provider>
+    <StripeProvider publishableKey="pk_test_51RKjdpFja6aePhuw7tr9FoJMw1W7eiuVmvQCwr4WLCHaR9GhVTC07WUrKdERoEZVE3tzdvLtETXvJbHgLKTDt79V003LshDDHf">
+      <Provider store={store}>
+        <Navigation />
+        <StatusBar style="auto" />
+      </Provider>
+    </StripeProvider>
   );
 }
 
