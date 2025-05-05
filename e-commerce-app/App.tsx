80201ed490ed53bd4990864cf6466193a654b217
Syed-Abdullah123 <abdullah.works.dev@gmail.com>
2025-05-05 12:18:38 +0500

    adjusted the imports and removed unncessary code

--- e-commerce-app/App.tsx
+++ e-commerce-app/App.tsx
@@ -1,24 +1,20 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import Navigation from "./src/navigation";
-import { Provider } from "react-redux";
-import { store } from "./src/store";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { StripeProvider } from "@stripe/stripe-react-native";
+import { Provider } from "react-redux";
+import { STRIPE_PUBLISHABLE_KEY } from "./src/config/constants";
+
+import { store } from "./src/store";
+import Navigation from "./src/navigation";
 
 export default function App() {
   return (
-    <StripeProvider publishableKey="pk_test_51RKjdpFja6aePhuw7tr9FoJMw1W7eiuVmvQCwr4WLCHaR9GhVTC07WUrKdERoEZVE3tzdvLtETXvJbHgLKTDt79V003LshDDHf">
-      <Provider store={store}>
+    <Provider store={store}>
+      <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
         <Navigation />
         <StatusBar style="auto" />
-      </Provider>
-    </StripeProvider>
+      </StripeProvider>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#ffffff",
-  },
-});
